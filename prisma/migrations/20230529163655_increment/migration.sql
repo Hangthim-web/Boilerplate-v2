f85cf5f5ad0364027a3b65e742e0e36643e3d3d5
hangthim limbu <hangthimlimbu123@gamil.com>
2023-05-31 08:26:55 +0545

    unfinished commit

---
+++ prisma/migrations/20230529163655_increment/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `viewCount` INTEGER NOT NULL DEFAULT 0;
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,3 @@
-// aVAr.delete('/todos/:id', (req: Request, res: Response) => {
-//     todos = todos.filter((todo) => todo.id !== req.params.id)
-//     res.sendStatus(200)
-// })
-
-// aVAr.patch('/todos/:id', (req: Request, res: Response) => {
-//     const { name } = req.body as TodoRequestBody
-//     const updatedTodos = todos.map((todo) =>
-//         todo.id === req.params.id ? { ...todo, name } : todo
-//     )
-//     todos = updatedTodos
-//     res.sendStatus(200)
-// })
-
-// aVAr.post('/todos', (req: Request, res: Response) => {
-//     const { name, active } = req.body as TodoRequestBody
-//     const newTodo: Todo = { id: crypto.randomUUID(), name, active }
-//     todos.push(newTodo)
-//     res.sendStatus(201)
-// })
-
 import aVar from './utils/create-server'
 
 const PORT = process.env.PORT || 3001
@@ -26,5 +5,3 @@ const PORT = process.env.PORT || 3001
 aVar.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`)
 })
-
-// console.log("hello");
